ebfc92f8849ddabe4736d4589bc6a3f58718ed09
bertolottimaxi <bertolottimaxi@gmail.com>
2022-03-09 20:51:17 -0300

    tamaño arreglado

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
-	backgroundColor: 0x6495ed,
+	backgroundColor: 0x649595,
 	width: 640,
 	height: 480
 });
@@ -16,12 +16,18 @@ Loader.shared.add({url: "./clampy.png", name: "myClampy"})
 Loader.shared.onComplete.add(()=>{
 	const clampy: Sprite = Sprite.from("myVaguito");
 
+	const proporción = clampy.width/clampy.height
+	
+	clampy.height=app.screen.height
+	clampy.width=clampy.height*proporción
+	
+
 	console.log("Hello World!", clampy.width,clampy.height)
 
 //clampy.anchor.set(0);
 
-clampy.x = 300 //app.screen.width / 2;
-clampy.y = 300 //app.screen.height / 2;
+clampy.x = 0 //app.screen.width / 2;
+clampy.y = 0 //app.screen.height / 2;
 
 app.stage.addChild(clampy);
 
